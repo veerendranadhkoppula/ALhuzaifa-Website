bf04bf0947434ecd80feff080ba4aa6eb88c781a
WhiteMantis143 <admin@whitemantis.ae>
2026-03-20 15:09:06 +0530

    fix: add

--- src/app/my-route/route.ts
+++ src/app/my-route/route.ts
@@ -2,7 +2,7 @@ import configPromise from '@payload-config'
 import { getPayload } from 'payload'
 
 export const GET = async (request: Request) => {
-  const payload = await getPayload({
+  const _payload = await getPayload({
     config: configPromise,
   })
 
