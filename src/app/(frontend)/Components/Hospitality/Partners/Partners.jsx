cbe753390386412780ec0e8304732c88231cbc0a
veerendranadhkoppula <veerendrakoppula68@gmail.com>
2026-04-02 10:14:52 +0530

    new veeeerrrrrsssssssssss

--- src/app/(frontend)/Components/Hospitality/Partners/Partners.jsx
+++ src/app/(frontend)/Components/Hospitality/Partners/Partners.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import styles from './Partners.module.css'
 import Image from 'next/image'
 import one from './1.png'
@@ -10,42 +10,68 @@ import five from './5.png'
 import six from './6.png'
 import { useTranslation } from '../../../hooks/useTranslation'
 
-const Partners = () => {
-    const { t } = useTranslation()
-  return (
-    <>
-      <div className={styles.main}>
-        <div className={styles.MainContainer}>
-          <div className={styles.left}>
-            <h3>{t.hospitalityPage.partnertitle}</h3>
-            <p>
-            {t.hospitalityPage.partnerDesc}
-            </p>
-          </div>
-          <div className={styles.right}>
-            <div className={styles.rowone}>
-              <Image src={one} alt="partner 1" className={styles.image} />
-              <Image src={two} alt="partner 2" className={styles.image} />
-              <Image src={three} alt="partner 3" className={styles.image} />
-              <Image src={four} alt="partner 4" className={styles.image} />
-            </div>
-            <div className={styles.rowtwo}>
-              <Image src={five} alt="partner 5" className={styles.image} />
-              <Image src={six} alt="partner 6" className={styles.image} />
-              <Image src={one} alt="partner 1" className={styles.image} />
-              <Image src={two} alt="partner 2" className={styles.image} />
-            </div>
-            <div className={styles.rowthree}>
-            <Image src={three} alt="partner 3" className={styles.image} />
-            <Image src={four} alt="partner 4" className={styles.image} />
-            <Image src={five} alt="partner 5" className={styles.image} />
-            <Image src={six} alt="partner 6" className={styles.image} />
-          </div>
-          </div>
-          
+const ALL_LOGOS = [one, two, three, four, five, six]
+
+const MarqueeRow = ({ logos, reverse = false, row3 = false }) => (
+  <div className={styles.marqueeWrapper}>
+    <div
+      className={`${styles.marqueeTrack} ${reverse ? styles.marqueeReverse : ''} ${row3 ? styles.marqueeRow3 : ''}`}
+    >
+      {[...logos, ...logos, ...logos, ...logos].map((logo, i) => (
+        <div key={i} className={styles.logoItem}>
+          <Image
+            src={logo}
+            alt={`partner ${(i % logos.length) + 1}`}
+            className={styles.logoImg}
+          />
         </div>
+      ))}
+    </div>
+  </div>
+)
+
+const Partners = () => {
+  const { t } = useTranslation()
+  const rightRef = useRef(null)
+
+  useEffect(() => {
+    const el = rightRef.current
+    if (!el) return
+    const update = () => {
+      el.style.setProperty('--right-width', `${el.offsetWidth}px`)
+    }
+    update()
+    const ro = new ResizeObserver(update)
+    ro.observe(el)
+    return () => ro.disconnect()
+  }, [])
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.MainContainer}>
+
+        <div className={styles.left}>
+          <h3>{t.residentialPage.partnertitle}</h3>
+          <p>{t.residentialPage.partnerDesc}</p>
+        </div>
+
+        <div className={styles.right} ref={rightRef}>
+          <div className={styles.rowWrap}>
+            <MarqueeRow logos={ALL_LOGOS} reverse={false} />
+            <div className={styles.divider} />
+          </div>
+          <div className={styles.rowWrap}>
+            <MarqueeRow logos={ALL_LOGOS} reverse={true} />
+            <div className={styles.divider} />
+          </div>
+          <div className={styles.rowWrap}>
+            <MarqueeRow logos={ALL_LOGOS} reverse={false} row3={true} />
+            <div className={styles.divider} />
+          </div>
+        </div>
+
       </div>
-    </>
+    </div>
   )
 }
 
