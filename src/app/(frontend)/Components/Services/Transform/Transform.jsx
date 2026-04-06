8bc38481562568bfdc8db5696fe8a30862adcf8c
veerendranadhkoppula <veerendrakoppula68@gmail.com>
2026-04-06 11:02:57 +0530

    effqfffff

--- src/app/(frontend)/Components/Services/Transform/Transform.jsx
+++ src/app/(frontend)/Components/Services/Transform/Transform.jsx
@@ -23,41 +23,41 @@ const Transform = () => {
           </div>
           <div className={styles.Cards}>
 
-            <div className={styles.Card}>
+            <Link href={`/${locale}/services/hospitality`} className={styles.Card}>
               <div className={styles.CardTop}>
                 <Image src={one} alt="concept" className={styles.CardImg} />
               </div>
-              <Link href={`/${locale}/services/hospitality`} className={styles.CardBottom}>
+              <div className={styles.CardBottom}>
                 <h4>{t.servicesPage.hospitality}</h4>
                 <svg width="17" height="14" viewBox="0 0 17 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M9.9227 12.4141L15.5312 6.91406L9.9227 1.41406M14.2566 6.91406L1 6.91406" stroke="#69594F" strokeWidth="2" strokeLinecap="square" />
                 </svg>
-              </Link>
-            </div>
+              </div>
+            </Link>
 
-            <div className={styles.Card}>
+            <Link href={`/${locale}/services/residential`} className={styles.Card}>
               <div className={styles.CardTop}>
                 <Image src={two} alt="concept" className={styles.CardImg} />
               </div>
-              <Link href={`/${locale}/services/residential`} className={styles.CardBottom}>
+              <div className={styles.CardBottom}>
                 <h4>{t.servicesPage.residential}</h4>
                 <svg width="17" height="14" viewBox="0 0 17 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M9.9227 12.4141L15.5312 6.91406L9.9227 1.41406M14.2566 6.91406L1 6.91406" stroke="#69594F" strokeWidth="2" strokeLinecap="square" />
                 </svg>
-              </Link>
-            </div>
+              </div>
+            </Link>
 
-            <div className={styles.Card}>
+            <Link href={`/${locale}/services/commercial`} className={styles.Card}>
               <div className={styles.CardTop}>
                 <Image src={three} alt="concept" className={styles.CardImg} />
               </div>
-              <Link href={`/${locale}/services/commercial`} className={styles.CardBottom}>
+              <div className={styles.CardBottom}>
                 <h4>{t.servicesPage.commercial}</h4>
                 <svg width="17" height="14" viewBox="0 0 17 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M9.9227 12.4141L15.5312 6.91406L9.9227 1.41406M14.2566 6.91406L1 6.91406" stroke="#69594F" strokeWidth="2" strokeLinecap="square" />
                 </svg>
-              </Link>
-            </div>
+              </div>
+            </Link>
 
           </div>
         </div>
