d76c97ca56cfcfc8ace483858d7add1afd587cd4
veerendranadhkoppula <veerendrakoppula68@gmail.com>
2026-04-10 11:38:13 +0530

    final touchsss

--- src/app/(frontend)/Components/Home/Landing/Landing.jsx
+++ src/app/(frontend)/Components/Home/Landing/Landing.jsx
@@ -10,33 +10,43 @@ const Landing = () => {
   const containerRef = useRef(null)
   const textRef = useRef(null)
 
-useEffect(() => {
-  const ctx = gsap.context(() => {
-    gsap.set(textRef.current, { opacity: 0, y: 40 })
-    gsap.set(containerRef.current, { scale: 1 })
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.set(textRef.current, { opacity: 0, y: 40 })
+      gsap.set(containerRef.current, { scale: 1 })
 
+      gsap.to(containerRef.current, {
+        scale: 1.05,
+        duration: 6,
+        ease: 'power2.out',
+      })
 
-    gsap.to(containerRef.current, {
-      scale: 1.05,
-      duration: 6,
-      ease: 'power2.out',
+      gsap.to(textRef.current, {
+        opacity: 1,
+        y: 0,
+        duration: 1.5,
+        delay: 0.5,
+        ease: 'power3.out',
+      })
     })
 
-
-    gsap.to(textRef.current, {
-      opacity: 1,
-      y: 0,
-      duration: 1.5,
-      delay: 0.5,
-      ease: 'power3.out',
-    })
-  })
-
-  return () => ctx.revert() 
-}, [])
+    return () => ctx.revert()
+  }, [])
 
   return (
     <div className={styles.Main}>
+
+      <video
+        className={styles.videoBg}
+        autoPlay
+        muted
+        loop
+        playsInline
+        //  poster="/homethumb.jpg"
+      >
+        <source src="/home.mp4" type="video/mp4" />
+      </video>
+
       <div ref={containerRef} className={styles.MainContainer}>
         <h3 ref={textRef}>{t.landing.heading}</h3>
       </div>
