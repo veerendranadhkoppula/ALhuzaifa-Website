dea1dcc3d13c59f263cd876a2e98c041bc7d421a
veerendranadhkoppula <veerendrakoppula68@gmail.com>
2026-04-10 11:51:34 +0530

    final touchsssddss

--- src/app/(frontend)/Components/Home/TextContainer/TextContainer.jsx
+++ src/app/(frontend)/Components/Home/TextContainer/TextContainer.jsx
@@ -1,47 +1,19 @@
 'use client'
-import React, { useEffect, useRef } from 'react'
+import React, { useRef } from 'react'
 import styles from './TextContainer.module.css'
 import { useTranslation } from '../../../hooks/useTranslation'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import SplitType from 'split-type'
+import { useSplitTextReveal } from '../../useSplitTextReveal/useSplitTextReveal'
 
-gsap.registerPlugin(ScrollTrigger)
+
+
 
 const TextContainer = () => {
   const { t } = useTranslation()
   const textRef = useRef(null)
 
- useEffect(() => {
-  const ctx = gsap.context(() => {
 
-   const split = new SplitType(textRef.current, {
-  types: 'lines',
-  tagName: 'span',
-   lineClass: 'line',
-})
 
-gsap.set(split.lines, { willChange: 'transform' })
-
-gsap.from(split.lines, {
-  yPercent: 100,
-  opacity: 0,
-  duration: 1.2,
-  ease: 'power4.out',
-  stagger: {
-    each: 0.12,
-    from: 'start',
-  },
-  scrollTrigger: {
-    trigger: textRef.current,
-    start: 'top 85%',
-  },
-})
-  })
-
-  return () => ctx.revert()
-}, [])
-
+useSplitTextReveal(textRef)
   return (
     <div className={styles.Main}>
       <div className={styles.MainContainer}>
